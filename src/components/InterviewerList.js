f5ac53fe8749e71e61c8a5a146d3ea5cf2348c09
habeshongo <doctorarferguson@gmail.com>
2023-05-29 23:55:02 -0300

    [create]-InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import InterviewerListItem from './InterviewerListItem';
+import "styles/variables.scss";
+
+
+
+const InterviewerList = (props) => {
+
+
+    const interviewerItems = props.interviewers.map((interviewer) => (
+        <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === props.value}
+            setInterviewer={() => props.onChange(interviewer.id)}
+
+        />
+    ));
+    return (
+        <section className="interviewers">
+            <h4 className="interviewers__header text--light">Interviewer</h4>
+            <ul className="interviewers__list">
+                {interviewerItems}
+            </ul>
+        </section>
+    )
+}
+
+
+export default InterviewerList;
