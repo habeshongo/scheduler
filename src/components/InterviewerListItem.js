075afebf7722c1dff1fc0edbca3f077a9e6d09e7
habeshongo <doctorarferguson@gmail.com>
2023-05-29 23:55:24 -0300

    [create]-InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import classNames from 'classnames';
+import './InterviewerListItem.scss'
+
+
+const InterviewerListItem = (props) => {
+
+    const interviewer = classNames('interviewers__item', {
+        'interviewers__item--selected': props.selected,
+    })
+
+    return (
+        <li className={interviewer} onClick={props.setInterviewer}>
+            <img
+                className="interviewers__item-image"
+                src={props.avatar}
+                alt={props.name}
+            />
+            {props.selected && props.name}
+        </li>
+
+    )
+}
+
+export default InterviewerListItem
+
+
+
+
+
+
