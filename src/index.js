48d9a78b1f8b1e3eabcc4e4c6af6535feefd7174
habeshongo <doctorarferguson@gmail.com>
2023-05-30 00:04:25 -0300

    [update]-import DayList component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "index.scss";
-
 import Application from "components/Application";
+import DayList from '../src/components/DayList';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+const days = [
+    {
+        id: 1,
+        name: "Monday",
+        spots: 2,
+    },
+    {
+        id: 2,
+        name: "Tuesday",
+        spots: 5,
+    },
+    {
+        id: 3,
+        name: "Wednesday",
+        spots: 0,
+    },
+];
+
+storiesOf("DayList", module)
+    .addParameters({
+        backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+        <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+        <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ))
+    .add("Wednesday", () => (
+        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    ));
